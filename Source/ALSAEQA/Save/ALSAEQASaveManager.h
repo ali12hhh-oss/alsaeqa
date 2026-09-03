f26ec1a8441d5192ee7f0284085565aefd9479bd
ali12hhh-oss <ali12.hhh@gmail.com>
2026-09-03 17:27:08 +0300

    إضافة مدير حفظ موحد للتقدم والـLegacy والرفيقة

---
+++ Source/ALSAEQA/Save/ALSAEQASaveManager.h
@@ -0,0 +1,58 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/GameInstanceSubsystem.h"
+#include "Companions/ALSAEQACompanionStoryTypes.h"
+#include "Save/ALSAEQASaveGame.h"
+#include "ALSAEQASaveManager.generated.h"
+
+UCLASS()
+class ALSAEQA_API UALSAEQASaveManager : public UGameInstanceSubsystem
+{
+    GENERATED_BODY()
+
+public:
+    UFUNCTION(BlueprintCallable, Category="ALSAEQA|Save")
+    bool SaveProgress();
+
+    UFUNCTION(BlueprintCallable, Category="ALSAEQA|Save")
+    bool LoadProgress();
+
+    UFUNCTION(BlueprintCallable, Category="ALSAEQA|Save")
+    void SetStage(int32 NewStage);
+
+    UFUNCTION(BlueprintPure, Category="ALSAEQA|Save")
+    int32 GetStage() const;
+
+    UFUNCTION(BlueprintCallable, Category="ALSAEQA|Legacy")
+    bool RegisterMemory(FName MemoryId);
+
+    UFUNCTION(BlueprintCallable, Category="ALSAEQA|Legacy")
+    bool RegisterWorldFlag(FName FlagId);
+
+    UFUNCTION(BlueprintPure, Category="ALSAEQA|Legacy")
+    bool HasMemory(FName MemoryId) const;
+
+    UFUNCTION(BlueprintPure, Category="ALSAEQA|Legacy")
+    bool HasWorldFlag(FName FlagId) const;
+
+    UFUNCTION(BlueprintCallable, Category="ALSAEQA|Companion")
+    bool AddFamilyClue(FName ClueId);
+
+    UFUNCTION(BlueprintCallable, Category="ALSAEQA|Companion")
+    bool CanBeginFamilySearch() const;
+
+    UFUNCTION(BlueprintPure, Category="ALSAEQA|Companion")
+    FALSAEQACompanionStoryProgress GetCompanionStory() const { return SaveData.CompanionStory; }
+
+    UFUNCTION(BlueprintCallable, Category="ALSAEQA|Companion")
+    bool SetCompanionState(EALSAEQACompanionStoryState NewState);
+
+private:
+    static const FString SaveSlotName;
+    static constexpr int32 FamilySearchStage = 25;
+    UPROPERTY()
+    UALSAEQASaveGame* SaveData = nullptr;
+
+    bool EnsureSaveData();
+};
