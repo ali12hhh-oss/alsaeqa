27ad344b9e0d1178e2f1e8dd3099896ae6fb2da7
ali12hhh-oss <ali12.hhh@gmail.com>
2026-09-03 18:36:39 +0300

    Wire enemy damage reception and death handling

--- Source/ALSAEQA/AI/ALSAEQAEnemyCharacter.cpp
+++ Source/ALSAEQA/AI/ALSAEQAEnemyCharacter.cpp
@@ -12,6 +12,10 @@ AALSAEQAEnemyCharacter::AALSAEQAEnemyCharacter()
 void AALSAEQAEnemyCharacter::BeginPlay()
 {
     Super::BeginPlay();
+    if (HealthComponent)
+    {
+        HealthComponent->OnDeath.AddDynamic(this, &AALSAEQAEnemyCharacter::HandleDeath);
+    }
 }
 
 void AALSAEQAEnemyCharacter::SetEnemyState(EALSAEQAEnemyState NewState)
@@ -31,4 +35,28 @@ void AALSAEQAEnemyCharacter::SetTargetActor(AActor* NewTarget)
     {
         SetEnemyState(EALSAEQAEnemyState::Alert);
     }
+}
+
+float AALSAEQAEnemyCharacter::ReceiveALSAEQADamage_Implementation(const FALSAEQADamageInfo& DamageInfo)
+{
+    if (!HealthComponent || HealthComponent->IsDead() || DamageInfo.Amount <= 0.0f)
+    {
+        return 0.0f;
+    }
+
+    HealthComponent->ApplyDamage(DamageInfo.Amount);
+    if (!HealthComponent->IsDead() && EnemyState != EALSAEQAEnemyState::Stunned)
+    {
+        SetEnemyState(EALSAEQAEnemyState::Alert);
+        if (DamageInfo.Instigator)
+        {
+            SetTargetActor(DamageInfo.Instigator);
+        }
+    }
+    return DamageInfo.Amount;
+}
+
+void AALSAEQAEnemyCharacter::HandleDeath()
+{
+    SetEnemyState(EALSAEQAEnemyState::Dead);
 }
