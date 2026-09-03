4a8f192114032ab065791e6621e9789779ffe9b4
ali12hhh-oss <ali12.hhh@gmail.com>
2026-09-03 17:07:21 +0300

    Make dynamic storm subsystem tickable and lifetime-safe

--- Source/ALSAEQA/Storm/ALSAEQADynamicStormSubsystem.h
+++ Source/ALSAEQA/Storm/ALSAEQADynamicStormSubsystem.h
@@ -2,18 +2,19 @@
 
 #include "CoreMinimal.h"
 #include "Subsystems/WorldSubsystem.h"
+#include "Tickable.h"
 #include "ALSAEQADynamicStormSubsystem.generated.h"
 
 DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FALSAEQAStormStateChanged, bool, bActive);
 
 UCLASS()
-class ALSAEQA_API UALSAEQADynamicStormSubsystem : public UWorldSubsystem
+class ALSAEQA_API UALSAEQADynamicStormSubsystem : public UWorldSubsystem, public FTickableGameObject
 {
     GENERATED_BODY()
 
 public:
     UFUNCTION(BlueprintCallable, Category="ALSAEQA|Storm")
-    void StartStorm(float DurationSeconds = 60.0f, float Intensity = 1.0f);
+    void StartStorm(float DurationSeconds = 60.0f, float StormIntensity = 1.0f);
 
     UFUNCTION(BlueprintCallable, Category="ALSAEQA|Storm")
     void StopStorm();
@@ -33,7 +34,9 @@ public:
 protected:
     virtual void Initialize(FSubsystemCollectionBase& Collection) override;
     virtual void Deinitialize() override;
-    virtual void Tick(float DeltaTime);
+    virtual void Tick(float DeltaTime) override;
+    virtual bool IsTickable() const override;
+    virtual TStatId GetStatId() const override;
 
 private:
     bool bStormActive = false;
