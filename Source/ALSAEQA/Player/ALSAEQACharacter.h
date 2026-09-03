bcf5d32f958dc6e4a3d017c1b875046f8aa42477
ali12hhh-oss <ali12.hhh@gmail.com>
2026-09-03 17:04:00 +0300

    Fix player ability component include path

--- Source/ALSAEQA/Player/ALSAEQACharacter.h
+++ Source/ALSAEQA/Player/ALSAEQACharacter.h
@@ -2,7 +2,7 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
-#include "Abilities/ALSAEQAAbilityComponent.h"
+#include "Systems/ALSAEQAAbilityComponent.h"
 #include "ALSAEQACharacter.generated.h"
 
 class UALSAEQAHealthComponent;
@@ -17,71 +17,32 @@ class ALSAEQA_API AALSAEQACharacter : public ACharacter
 
 public:
     AALSAEQACharacter();
-
     virtual void SetupPlayerInputComponent(UInputComponent* PlayerInputComponent) override;
-
-    UFUNCTION(BlueprintCallable, Category="ALSAEQA|Combat")
-    void PerformLightAttack();
-
-    UFUNCTION(BlueprintCallable, Category="ALSAEQA|Combat")
-    void PerformHeavyAttack();
-
-    UFUNCTION(BlueprintCallable, Category="ALSAEQA|Thunder")
-    void BeginThunderCharge();
-
-    UFUNCTION(BlueprintCallable, Category="ALSAEQA|Thunder")
-    void ReleaseThunderCharge();
-
-    UFUNCTION(BlueprintCallable, Category="ALSAEQA|Thunder")
-    void CancelThunderCharge();
-
-    UFUNCTION(BlueprintCallable, Category="ALSAEQA|Movement")
-    void StartSprint();
-
-    UFUNCTION(BlueprintCallable, Category="ALSAEQA|Movement")
-    void StopSprint();
-
-    UFUNCTION(BlueprintCallable, Category="ALSAEQA|Abilities")
-    bool ActivateAbility(EALSAEQAAbility Ability);
-
-    UFUNCTION(BlueprintPure, Category="ALSAEQA|Abilities")
-    UALSAEQAAbilityComponent* GetAbilityComponent() const { return AbilityComponent; }
-
-    UFUNCTION(BlueprintPure, Category="ALSAEQA|Health")
-    UALSAEQAHealthComponent* GetHealthComponent() const { return HealthComponent; }
-
-    UFUNCTION(BlueprintPure, Category="ALSAEQA|Thunder")
-    UALSAEQAThunderChargeComponent* GetThunderChargeComponent() const { return ThunderChargeComponent; }
+    UFUNCTION(BlueprintCallable, Category="ALSAEQA|Combat") void PerformLightAttack();
+    UFUNCTION(BlueprintCallable, Category="ALSAEQA|Combat") void PerformHeavyAttack();
+    UFUNCTION(BlueprintCallable, Category="ALSAEQA|Thunder") void BeginThunderCharge();
+    UFUNCTION(BlueprintCallable, Category="ALSAEQA|Thunder") void ReleaseThunderCharge();
+    UFUNCTION(BlueprintCallable, Category="ALSAEQA|Thunder") void CancelThunderCharge();
+    UFUNCTION(BlueprintCallable, Category="ALSAEQA|Movement") void StartSprint();
+    UFUNCTION(BlueprintCallable, Category="ALSAEQA|Movement") void StopSprint();
+    UFUNCTION(BlueprintCallable, Category="ALSAEQA|Abilities") bool ActivateAbility(EALSAEQAAbility Ability);
+    UFUNCTION(BlueprintPure, Category="ALSAEQA|Abilities") UALSAEQAAbilityComponent* GetAbilityComponent() const { return AbilityComponent; }
+    UFUNCTION(BlueprintPure, Category="ALSAEQA|Health") UALSAEQAHealthComponent* GetHealthComponent() const { return HealthComponent; }
+    UFUNCTION(BlueprintPure, Category="ALSAEQA|Thunder") UALSAEQAThunderChargeComponent* GetThunderChargeComponent() const { return ThunderChargeComponent; }
 
 protected:
     virtual void BeginPlay() override;
-
     void MoveForward(float Value);
     void MoveRight(float Value);
     void LookUp(float Value);
     void Turn(float Value);
 
-    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="ALSAEQA|Components")
-    TObjectPtr<USpringArmComponent> CameraBoom;
-
-    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="ALSAEQA|Components")
-    TObjectPtr<UCameraComponent> FollowCamera;
-
-    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="ALSAEQA|Components")
-    TObjectPtr<UALSAEQAAbilityComponent> AbilityComponent;
-
-    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="ALSAEQA|Components")
-    TObjectPtr<UALSAEQAHealthComponent> HealthComponent;
-
-    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="ALSAEQA|Components")
-    TObjectPtr<UALSAEQAThunderChargeComponent> ThunderChargeComponent;
-
-    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="ALSAEQA|Movement")
-    float WalkSpeed = 360.0f;
-
-    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="ALSAEQA|Movement")
-    float SprintSpeed = 620.0f;
-
-    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="ALSAEQA|Thunder")
-    float ThunderReleaseDamage = 30.0f;
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="ALSAEQA|Components") TObjectPtr<USpringArmComponent> CameraBoom;
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="ALSAEQA|Components") TObjectPtr<UCameraComponent> FollowCamera;
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="ALSAEQA|Components") TObjectPtr<UALSAEQAAbilityComponent> AbilityComponent;
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="ALSAEQA|Components") TObjectPtr<UALSAEQAHealthComponent> HealthComponent;
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="ALSAEQA|Components") TObjectPtr<UALSAEQAThunderChargeComponent> ThunderChargeComponent;
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="ALSAEQA|Movement") float WalkSpeed = 360.0f;
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="ALSAEQA|Movement") float SprintSpeed = 620.0f;
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="ALSAEQA|Thunder") float ThunderReleaseDamage = 30.0f;
 };
