b0fc16878b846f518e0a92ab6251412b46be4929
ali12hhh-oss <ali12.hhh@gmail.com>
2026-09-03 18:31:48 +0300

    Integrate specialized mount abilities

--- Source/ALSAEQA/Companions/ALSAEQAMountActor.h
+++ Source/ALSAEQA/Companions/ALSAEQAMountActor.h
@@ -6,6 +6,7 @@
 #include "ALSAEQAMountActor.generated.h"
 
 class UALSAEQAMountComponent;
+class UALSAEQAMountAbilityComponent;
 
 UCLASS(BlueprintType, Blueprintable)
 class ALSAEQA_API AALSAEQAMountActor : public ACharacter
@@ -14,72 +15,33 @@ class ALSAEQA_API AALSAEQAMountActor : public ACharacter
 
 public:
     AALSAEQAMountActor();
-
     virtual void Tick(float DeltaSeconds) override;
 
-    UFUNCTION(BlueprintCallable, Category="ALSAEQA|Mount")
-    bool InitializeWildMount(const FALSAEQAMountProfile& Profile);
+    UFUNCTION(BlueprintCallable, Category="ALSAEQA|Mount") bool InitializeWildMount(const FALSAEQAMountProfile& Profile);
+    UFUNCTION(BlueprintCallable, Category="ALSAEQA|Mount") bool BeginTaming();
+    UFUNCTION(BlueprintCallable, Category="ALSAEQA|Mount") bool AddTamingProgress(float Amount);
+    UFUNCTION(BlueprintCallable, Category="ALSAEQA|Mount") bool Tame(const FALSAEQAMountProfile& Profile);
+    UFUNCTION(BlueprintCallable, Category="ALSAEQA|Mount") bool MountRider(AActor* Rider);
+    UFUNCTION(BlueprintCallable, Category="ALSAEQA|Mount") bool DismountRider();
+    UFUNCTION(BlueprintCallable, Category="ALSAEQA|Mount") bool InjureMount();
+    UFUNCTION(BlueprintCallable, Category="ALSAEQA|Mount") bool ReleaseMount();
+    UFUNCTION(BlueprintCallable, Category="ALSAEQA|Mount") bool MoveRiderForward(float Value);
+    UFUNCTION(BlueprintCallable, Category="ALSAEQA|Mount") bool MoveRiderRight(float Value);
+    UFUNCTION(BlueprintCallable, Category="ALSAEQA|Mount") bool SetRiderSprint(bool bEnabled);
 
-    UFUNCTION(BlueprintCallable, Category="ALSAEQA|Mount")
-    bool BeginTaming();
-
-    UFUNCTION(BlueprintCallable, Category="ALSAEQA|Mount")
-    bool AddTamingProgress(float Amount);
-
-    UFUNCTION(BlueprintCallable, Category="ALSAEQA|Mount")
-    bool Tame(const FALSAEQAMountProfile& Profile);
-
-    UFUNCTION(BlueprintCallable, Category="ALSAEQA|Mount")
-    bool MountRider(AActor* Rider);
-
-    UFUNCTION(BlueprintCallable, Category="ALSAEQA|Mount")
-    bool DismountRider();
-
-    UFUNCTION(BlueprintCallable, Category="ALSAEQA|Mount")
-    bool InjureMount();
-
-    UFUNCTION(BlueprintCallable, Category="ALSAEQA|Mount")
-    bool ReleaseMount();
-
-    UFUNCTION(BlueprintCallable, Category="ALSAEQA|Mount")
-    bool MoveRiderForward(float Value);
-
-    UFUNCTION(BlueprintCallable, Category="ALSAEQA|Mount")
-    bool MoveRiderRight(float Value);
-
-    UFUNCTION(BlueprintCallable, Category="ALSAEQA|Mount")
-    bool SetRiderSprint(bool bEnabled);
-
-    UFUNCTION(BlueprintPure, Category="ALSAEQA|Mount")
-    bool HasRider() const { return Rider.IsValid(); }
-
-    UFUNCTION(BlueprintPure, Category="ALSAEQA|Mount")
-    bool IsRiderSprinting() const { return bRiderSprinting; }
-
-    UFUNCTION(BlueprintPure, Category="ALSAEQA|Mount")
-    UALSAEQAMountComponent* GetMountComponent() const { return MountComponent; }
-
-    UFUNCTION(BlueprintPure, Category="ALSAEQA|Mount")
-    AActor* GetRider() const { return Rider.Get(); }
+    UFUNCTION(BlueprintPure, Category="ALSAEQA|Mount") bool HasRider() const { return Rider.IsValid(); }
+    UFUNCTION(BlueprintPure, Category="ALSAEQA|Mount") bool IsRiderSprinting() const { return bRiderSprinting; }
+    UFUNCTION(BlueprintPure, Category="ALSAEQA|Mount") UALSAEQAMountComponent* GetMountComponent() const { return MountComponent; }
+    UFUNCTION(BlueprintPure, Category="ALSAEQA|Mount") UALSAEQAMountAbilityComponent* GetMountAbilityComponent() const { return MountAbilityComponent; }
+    UFUNCTION(BlueprintPure, Category="ALSAEQA|Mount") AActor* GetRider() const { return Rider.Get(); }
 
 protected:
-    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="ALSAEQA|Mount")
-    TObjectPtr<UALSAEQAMountComponent> MountComponent;
-
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="ALSAEQA|Mount")
-    FVector RiderOffset = FVector(0.0f, 0.0f, 90.0f);
-
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="ALSAEQA|Mount")
-    float SprintSpeedMultiplier = 1.35f;
-
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="ALSAEQA|Mount")
-    float StaminaPerSecondWhileSprinting = 8.0f;
-
-    UPROPERTY(BlueprintReadOnly, Category="ALSAEQA|Mount")
-    TWeakObjectPtr<AActor> Rider;
-
-    UPROPERTY(BlueprintReadOnly, Category="ALSAEQA|Mount")
-    bool bRiderSprinting = false;
-
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="ALSAEQA|Mount") TObjectPtr<UALSAEQAMountComponent> MountComponent;
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="ALSAEQA|Mount") TObjectPtr<UALSAEQAMountAbilityComponent> MountAbilityComponent;
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="ALSAEQA|Mount") FVector RiderOffset = FVector(0.0f, 0.0f, 90.0f);
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="ALSAEQA|Mount") float SprintSpeedMultiplier = 1.35f;
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="ALSAEQA|Mount") float StaminaPerSecondWhileSprinting = 8.0f;
+    UPROPERTY(BlueprintReadOnly, Category="ALSAEQA|Mount") TWeakObjectPtr<AActor> Rider;
+    UPROPERTY(BlueprintReadOnly, Category="ALSAEQA|Mount") bool bRiderSprinting = false;
     float BaseMovementSpeed = 650.0f;
 };
