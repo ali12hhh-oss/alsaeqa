d56929648d149e53b4cc2f59951b4adae9a442dd
ali12hhh-oss <ali12.hhh@gmail.com>
2026-09-03 18:01:01 +0300

    feat: add mount actor gameplay foundation

---
+++ Source/ALSAEQA/Companions/ALSAEQAMountActor.h
@@ -0,0 +1,45 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "ALSAEQAMountTypes.h"
+#include "ALSAEQAMountActor.generated.h"
+
+class UALSAEQAMountComponent;
+
+UCLASS(BlueprintType, Blueprintable)
+class ALSAEQA_API AALSAEQAMountActor : public ACharacter
+{
+    GENERATED_BODY()
+
+public:
+    AALSAEQAMountActor();
+
+    UFUNCTION(BlueprintCallable, Category="ALSAEQA|Mount")
+    bool Tame(const FALSAEQAMountProfile& Profile);
+
+    UFUNCTION(BlueprintCallable, Category="ALSAEQA|Mount")
+    bool MountRider(AActor* Rider);
+
+    UFUNCTION(BlueprintCallable, Category="ALSAEQA|Mount")
+    bool DismountRider();
+
+    UFUNCTION(BlueprintPure, Category="ALSAEQA|Mount")
+    bool HasRider() const { return Rider.IsValid(); }
+
+    UFUNCTION(BlueprintPure, Category="ALSAEQA|Mount")
+    UALSAEQAMountComponent* GetMountComponent() const { return MountComponent; }
+
+    UFUNCTION(BlueprintPure, Category="ALSAEQA|Mount")
+    AActor* GetRider() const { return Rider.Get(); }
+
+protected:
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="ALSAEQA|Mount")
+    TObjectPtr<UALSAEQAMountComponent> MountComponent;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="ALSAEQA|Mount")
+    FVector RiderOffset = FVector(0.0f, 0.0f, 90.0f);
+
+    UPROPERTY(BlueprintReadOnly, Category="ALSAEQA|Mount")
+    TWeakObjectPtr<AActor> Rider;
+};
