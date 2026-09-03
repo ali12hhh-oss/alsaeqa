4c9da2c2c78461afe4cd910559b11fd3bf3fe8b6
ali12hhh-oss <ali12.hhh@gmail.com>
2026-09-03 20:58:15 +0300

    Replace primitive visuals with real generated 3D character meshes

--- Source/ALSAEQA/Visual/ALSAEQARuntimeVisualComponent.h
+++ Source/ALSAEQA/Visual/ALSAEQARuntimeVisualComponent.h
@@ -2,24 +2,30 @@
 #include "CoreMinimal.h"
 #include "Components/ActorComponent.h"
 #include "ALSAEQARuntimeVisualComponent.generated.h"
-UENUM(BlueprintType) enum class EALSAEQAVisualRole:uint8{Hero,Companion,Enemy,Boss,Snake,Mount,Weapon};
-UCLASS(ClassGroup=(ALSAEQA),meta=(BlueprintSpawnableComponent)) class ALSAEQA_API UALSAEQARuntimeVisualComponent:public UActorComponent
+
+UENUM(BlueprintType)
+enum class EALSAEQAVisualRole : uint8 { Hero, Companion, Enemy, Boss, Snake, Mount, Weapon };
+
+UCLASS(ClassGroup=(ALSAEQA), meta=(BlueprintSpawnableComponent))
+class ALSAEQA_API UALSAEQARuntimeVisualComponent : public UActorComponent
 {
  GENERATED_BODY()
 public:
  UALSAEQARuntimeVisualComponent();
  UFUNCTION(BlueprintCallable,Category="ALSAEQA|Visual") void RebuildVisual();
  UFUNCTION(BlueprintCallable,Category="ALSAEQA|Visual") void SetVisualRole(EALSAEQAVisualRole NewRole);
- UFUNCTION(BlueprintPure,Category="ALSAEQA|Visual") EALSAEQAVisualRole GetVisualRole()const{return VisualRole;}
+ UFUNCTION(BlueprintPure,Category="ALSAEQA|Visual") EALSAEQAVisualRole GetVisualRole() const{return VisualRole;}
 protected:
- virtual void BeginPlay()override;
- virtual void TickComponent(float DeltaTime,ELevelTick TickType,FActorComponentTickFunction* ThisTickFunction)override;
+ virtual void BeginPlay() override; virtual void TickComponent(float Delta,ELevelTick TickType,FActorComponentTickFunction* ThisTickFunction) override;
  UPROPERTY(EditAnywhere,BlueprintReadWrite,Category="ALSAEQA|Visual") EALSAEQAVisualRole VisualRole=EALSAEQAVisualRole::Hero;
  UPROPERTY(EditAnywhere,BlueprintReadWrite,Category="ALSAEQA|Visual") bool bBuildOnBeginPlay=true;
  UPROPERTY(EditAnywhere,BlueprintReadWrite,Category="ALSAEQA|Visual") bool bAddWeapon=true;
  UPROPERTY(EditAnywhere,BlueprintReadWrite,Category="ALSAEQA|Visual") bool bAddStormLight=true;
 private:
- void ClearVisual(); void AddMesh(const TCHAR* AssetPath,const FVector& RelativeLocation,const FVector& RelativeScale,const FRotator& RelativeRotation); void AddWeaponVisual(); void AddStormVisual();
- UPROPERTY(Transient) TArray<TObjectPtr<USceneComponent>> VisualParts;
- float VisualTime=0.0f;
+ void ClearVisual(); void AddMeshPart(const TArray<FVector>&V,const TArray<int32>&T,const FVector&Loc,const FRotator&Rot,const FVector&Scale,const FLinearColor&Color,bool Animate=false);
+ void AddEllipsoid(const FVector&Loc,const FVector&Scale,const FLinearColor&Color,bool Animate=false,int32 Seg=14,int32 Rings=7);
+ void AddTube(const FVector&Loc,float R0,float R1,float H,const FVector&Scale,const FLinearColor&Color,const FRotator&Rot=FRotator::ZeroRotator,bool Animate=false,int32 Seg=12);
+ void AddWeaponVisual(); void AddStormVisual();
+ UPROPERTY(Transient) TArray<TObjectPtr<USceneComponent>> VisualParts; UPROPERTY(Transient) TArray<TObjectPtr<USceneComponent>> AnimatedParts;
+ float AnimTime=0.f;
 };
