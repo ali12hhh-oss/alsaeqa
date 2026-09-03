716489c56659eeb998fe683a0019f3c76e00f550
ali12hhh-oss <ali12.hhh@gmail.com>
2026-09-03 17:03:02 +0300

    Complete player thunder charge integration

--- Source/ALSAEQA/Player/ALSAEQACharacter.cpp
+++ Source/ALSAEQA/Player/ALSAEQACharacter.cpp
@@ -1,28 +1,26 @@
 #include "Player/ALSAEQACharacter.h"
-#include "Systems/ALSAEQAAbilityComponent.h"
 #include "Systems/ALSAEQAHealthComponent.h"
+#include "Storm/ALSAEQAThunderChargeComponent.h"
 #include "Camera/CameraComponent.h"
 #include "GameFramework/SpringArmComponent.h"
 #include "Components/InputComponent.h"
+#include "Combat/ALSAEQADamageTypes.h"
 
 AALSAEQACharacter::AALSAEQACharacter()
 {
     PrimaryActorTick.bCanEverTick = true;
-
     CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
     CameraBoom->SetupAttachment(RootComponent);
     CameraBoom->TargetArmLength = 360.0f;
     CameraBoom->bUsePawnControlRotation = true;
     CameraBoom->bEnableCameraLag = true;
     CameraBoom->CameraLagSpeed = 12.0f;
-
     FollowCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("FollowCamera"));
     FollowCamera->SetupAttachment(CameraBoom, USpringArmComponent::SocketName);
     FollowCamera->bUsePawnControlRotation = false;
-
     AbilityComponent = CreateDefaultSubobject<UALSAEQAAbilityComponent>(TEXT("AbilityComponent"));
     HealthComponent = CreateDefaultSubobject<UALSAEQAHealthComponent>(TEXT("HealthComponent"));
-
+    ThunderChargeComponent = CreateDefaultSubobject<UALSAEQAThunderChargeComponent>(TEXT("ThunderChargeComponent"));
     GetCharacterMovement()->MaxWalkSpeed = WalkSpeed;
     GetCharacterMovement()->BrakingDecelerationWalking = 1800.0f;
     GetCharacterMovement()->AirControl = 0.35f;
@@ -39,7 +37,6 @@ void AALSAEQACharacter::BeginPlay()
 void AALSAEQACharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
     Super::SetupPlayerInputComponent(PlayerInputComponent);
-
     PlayerInputComponent->BindAxis(TEXT("MoveForward"), this, &AALSAEQACharacter::MoveForward);
     PlayerInputComponent->BindAxis(TEXT("MoveRight"), this, &AALSAEQACharacter::MoveRight);
     PlayerInputComponent->BindAxis(TEXT("LookUp"), this, &AALSAEQACharacter::LookUp);
@@ -48,6 +45,8 @@ void AALSAEQACharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputCo
     PlayerInputComponent->BindAction(TEXT("HeavyAttack"), IE_Pressed, this, &AALSAEQACharacter::PerformHeavyAttack);
     PlayerInputComponent->BindAction(TEXT("Sprint"), IE_Pressed, this, &AALSAEQACharacter::StartSprint);
     PlayerInputComponent->BindAction(TEXT("Sprint"), IE_Released, this, &AALSAEQACharacter::StopSprint);
+    PlayerInputComponent->BindAction(TEXT("ThunderCharge"), IE_Pressed, this, &AALSAEQACharacter::BeginThunderCharge);
+    PlayerInputComponent->BindAction(TEXT("ThunderCharge"), IE_Released, this, &AALSAEQACharacter::ReleaseThunderCharge);
 }
 
 void AALSAEQACharacter::MoveForward(float Value)
@@ -70,35 +69,56 @@ void AALSAEQACharacter::MoveRight(float Value)
     }
 }
 
-void AALSAEQACharacter::LookUp(float Value)
+void AALSAEQACharacter::LookUp(float Value) { AddControllerPitchInput(Value); }
+void AALSAEQACharacter::Turn(float Value) { AddControllerYawInput(Value); }
+void AALSAEQACharacter::StartSprint() { GetCharacterMovement()->MaxWalkSpeed = SprintSpeed; }
+void AALSAEQACharacter::StopSprint() { GetCharacterMovement()->MaxWalkSpeed = WalkSpeed; }
+void AALSAEQACharacter::PerformLightAttack() {}
+void AALSAEQACharacter::PerformHeavyAttack() {}
+
+void AALSAEQACharacter::BeginThunderCharge()
 {
-    AddControllerPitchInput(Value);
+    if (ThunderChargeComponent && AbilityComponent && AbilityComponent->HasAbility(EALSAEQAAbility::ThunderShock))
+    {
+        ThunderChargeComponent->BeginCharge();
+    }
 }
 
-void AALSAEQACharacter::Turn(float Value)
+void AALSAEQACharacter::ReleaseThunderCharge()
 {
-    AddControllerYawInput(Value);
+    if (!ThunderChargeComponent || !AbilityComponent || !AbilityComponent->HasAbility(EALSAEQAAbility::ThunderShock))
+    {
+        return;
+    }
+
+    const float ChargePercent = ThunderChargeComponent->GetChargePercent();
+    if (ChargePercent < 0.15f || AbilityComponent->GetEnergy() < ThunderShockEnergyCost)
+    {
+        ThunderChargeComponent->CancelCharge();
+        return;
+    }
+
+    const float ReleasedPercent = ThunderChargeComponent->ReleaseCharge();
+    if (ReleasedPercent <= 0.0f)
+    {
+        return;
+    }
+
+    AbilityComponent->ConsumeEnergy(ThunderShockEnergyCost);
+
+    const FALSAEQADamageInfo DamageInfo{
+        ThunderReleaseDamage * ThunderChargeComponent->GetReleasedDamageMultiplier(ReleasedPercent),
+        EALSAEQADamageType::Thunder,
+        this,
+        GetActorLocation() + GetActorForwardVector() * 120.0f
+    };
+    LastThunderDamage = DamageInfo.Amount;
+    LastThunderDamageInfo = DamageInfo;
 }
 
-void AALSAEQACharacter::StartSprint()
+void AALSAEQACharacter::CancelThunderCharge()
 {
-    GetCharacterMovement()->MaxWalkSpeed = SprintSpeed;
-}
-
-void AALSAEQACharacter::StopSprint()
-{
-    GetCharacterMovement()->MaxWalkSpeed = WalkSpeed;
-}
-
-void AALSAEQACharacter::PerformLightAttack()
-{
-    // Animation, hit detection and damage execution are intentionally data-driven
-    // and will be connected to the combat system without hard-coding assets here.
-}
-
-void AALSAEQACharacter::PerformHeavyAttack()
-{
-    // Heavy attack hook for the combat state machine.
+    if (ThunderChargeComponent) { ThunderChargeComponent->CancelCharge(); }
 }
 
 bool AALSAEQACharacter::ActivateAbility(EALSAEQAAbility Ability)
