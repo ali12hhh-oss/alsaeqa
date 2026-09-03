61ccd509e8dcf28b42e93bf09fd7fe8178162765
ali12hhh-oss <ali12.hhh@gmail.com>
2026-09-03 18:39:11 +0300

    Make the permanent companion an active partner after family rescue

--- Source/ALSAEQA/Companions/ALSAEQACompanionCharacter.h
+++ Source/ALSAEQA/Companions/ALSAEQACompanionCharacter.h
@@ -29,67 +29,35 @@ class ALSAEQA_API AALSAEQACompanionCharacter : public ACharacter
 public:
     AALSAEQACompanionCharacter();
 
-    UFUNCTION(BlueprintCallable, Category="ALSAEQA|Companion")
-    void SetBehaviorState(EALSAEQACompanionBehaviorState NewState);
+    UFUNCTION(BlueprintCallable, Category="ALSAEQA|Companion") void SetBehaviorState(EALSAEQACompanionBehaviorState NewState);
+    UFUNCTION(BlueprintCallable, Category="ALSAEQA|Companion") void SetCompanionId(FName NewCompanionId);
+    UFUNCTION(BlueprintCallable, Category="ALSAEQA|Companion") bool CaptureCompanion();
+    UFUNCTION(BlueprintCallable, Category="ALSAEQA|Companion") bool MarkRescueKnown();
+    UFUNCTION(BlueprintCallable, Category="ALSAEQA|Companion") bool RescueCompanion();
+    UFUNCTION(BlueprintCallable, Category="ALSAEQA|Companion") bool BeginFamilySearch();
 
-    UFUNCTION(BlueprintCallable, Category="ALSAEQA|Companion")
-    void SetCompanionId(FName NewCompanionId);
+    UFUNCTION(BlueprintCallable, Category="ALSAEQA|Companion|Hero Search")
+    bool BeginHelpingHeroFamilySearch();
 
-    UFUNCTION(BlueprintCallable, Category="ALSAEQA|Companion")
-    bool CaptureCompanion();
+    UFUNCTION(BlueprintPure, Category="ALSAEQA|Companion|Hero Search")
+    bool IsHelpingHeroFamilySearch() const { return bHelpingHeroFamilySearch; }
 
-    UFUNCTION(BlueprintCallable, Category="ALSAEQA|Companion")
-    bool MarkRescueKnown();
-
-    UFUNCTION(BlueprintCallable, Category="ALSAEQA|Companion")
-    bool RescueCompanion();
-
-    UFUNCTION(BlueprintCallable, Category="ALSAEQA|Companion")
-    bool BeginFamilySearch();
-
-    UFUNCTION(BlueprintCallable, Category="ALSAEQA|Riding")
-    bool Mount(AALSAEQAMountActor* MountActor);
-
-    UFUNCTION(BlueprintCallable, Category="ALSAEQA|Riding")
-    bool Dismount();
-
-    UFUNCTION(BlueprintPure, Category="ALSAEQA|Companion")
-    FName GetCompanionId() const { return CompanionId; }
-
-    UFUNCTION(BlueprintPure, Category="ALSAEQA|Companion")
-    EALSAEQACompanionBehaviorState GetBehaviorState() const { return BehaviorState; }
-
-    UFUNCTION(BlueprintPure, Category="ALSAEQA|Companion")
-    UALSAEQACompanionStoryComponent* GetStoryComponent() const { return StoryComponent; }
-
-    UFUNCTION(BlueprintPure, Category="ALSAEQA|Companion")
-    UALSAEQACompanionCaptureComponent* GetCaptureComponent() const { return CaptureComponent; }
-
-    UFUNCTION(BlueprintPure, Category="ALSAEQA|Companion")
-    UALSAEQARidingComponent* GetRidingComponent() const { return RidingComponent; }
+    UFUNCTION(BlueprintCallable, Category="ALSAEQA|Riding") bool Mount(AALSAEQAMountActor* MountActor);
+    UFUNCTION(BlueprintCallable, Category="ALSAEQA|Riding") bool Dismount();
+    UFUNCTION(BlueprintPure, Category="ALSAEQA|Companion") FName GetCompanionId() const { return CompanionId; }
+    UFUNCTION(BlueprintPure, Category="ALSAEQA|Companion") EALSAEQACompanionBehaviorState GetBehaviorState() const { return BehaviorState; }
+    UFUNCTION(BlueprintPure, Category="ALSAEQA|Companion") UALSAEQACompanionStoryComponent* GetStoryComponent() const { return StoryComponent; }
+    UFUNCTION(BlueprintPure, Category="ALSAEQA|Companion") UALSAEQACompanionCaptureComponent* GetCaptureComponent() const { return CaptureComponent; }
+    UFUNCTION(BlueprintPure, Category="ALSAEQA|Companion") UALSAEQARidingComponent* GetRidingComponent() const { return RidingComponent; }
 
 protected:
-    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="ALSAEQA|Companion")
-    TObjectPtr<UALSAEQAHealthComponent> HealthComponent;
-
-    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="ALSAEQA|Companion")
-    TObjectPtr<UALSAEQACompanionStoryComponent> StoryComponent;
-
-    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="ALSAEQA|Companion")
-    TObjectPtr<UALSAEQACompanionCaptureComponent> CaptureComponent;
-
-    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="ALSAEQA|Companion")
-    TObjectPtr<UALSAEQARidingComponent> RidingComponent;
-
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, SaveGame, Category="ALSAEQA|Companion")
-    FName CompanionId = TEXT("PermanentCompanion");
-
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="ALSAEQA|Companion")
-    float FollowDistance = 220.0f;
-
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="ALSAEQA|Companion")
-    float AssistRange = 900.0f;
-
-    UPROPERTY(BlueprintReadOnly, Category="ALSAEQA|Companion")
-    EALSAEQACompanionBehaviorState BehaviorState = EALSAEQACompanionBehaviorState::Following;
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="ALSAEQA|Companion") TObjectPtr<UALSAEQAHealthComponent> HealthComponent;
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="ALSAEQA|Companion") TObjectPtr<UALSAEQACompanionStoryComponent> StoryComponent;
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="ALSAEQA|Companion") TObjectPtr<UALSAEQACompanionCaptureComponent> CaptureComponent;
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="ALSAEQA|Companion") TObjectPtr<UALSAEQARidingComponent> RidingComponent;
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, SaveGame, Category="ALSAEQA|Companion") FName CompanionId = TEXT("PermanentCompanion");
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="ALSAEQA|Companion") float FollowDistance = 220.0f;
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="ALSAEQA|Companion") float AssistRange = 900.0f;
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, SaveGame, Category="ALSAEQA|Companion|Hero Search") bool bHelpingHeroFamilySearch = false;
+    UPROPERTY(BlueprintReadOnly, Category="ALSAEQA|Companion") EALSAEQACompanionBehaviorState BehaviorState = EALSAEQACompanionBehaviorState::Following;
 };
