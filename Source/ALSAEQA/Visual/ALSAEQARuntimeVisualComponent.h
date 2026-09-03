f8f0e8453ef689592c47f0ef7ec72b1827d85bba
ali12hhh-oss <ali12.hhh@gmail.com>
2026-09-03 20:50:42 +0300

    feat(visual): add runtime character and weapon visual framework

---
+++ Source/ALSAEQA/Visual/ALSAEQARuntimeVisualComponent.h
@@ -0,0 +1,43 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "ALSAEQARuntimeVisualComponent.generated.h"
+
+UENUM(BlueprintType)
+enum class EALSAEQAVisualRole : uint8
+{
+    Hero,
+    Companion,
+    Enemy,
+    Boss,
+    Snake,
+    Mount,
+    Weapon
+};
+
+UCLASS(ClassGroup=(ALSAEQA), meta=(BlueprintSpawnableComponent))
+class ALSAEQA_API UALSAEQARuntimeVisualComponent : public UActorComponent
+{
+    GENERATED_BODY()
+
+public:
+    UALSAEQARuntimeVisualComponent();
+    UFUNCTION(BlueprintCallable, Category="ALSAEQA|Visual") void RebuildVisual();
+    UFUNCTION(BlueprintCallable, Category="ALSAEQA|Visual") void SetVisualRole(EALSAEQAVisualRole NewRole);
+    UFUNCTION(BlueprintPure, Category="ALSAEQA|Visual") EALSAEQAVisualRole GetVisualRole() const { return VisualRole; }
+
+protected:
+    virtual void BeginPlay() override;
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="ALSAEQA|Visual") EALSAEQAVisualRole VisualRole = EALSAEQAVisualRole::Hero;
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="ALSAEQA|Visual") bool bBuildOnBeginPlay = true;
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="ALSAEQA|Visual") bool bAddWeapon = true;
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="ALSAEQA|Visual") bool bAddStormLight = true;
+
+private:
+    void ClearVisual();
+    void AddMesh(const TCHAR* AssetPath, const FVector& RelativeLocation, const FVector& RelativeScale, const FRotator& RelativeRotation);
+    void AddWeaponVisual();
+    void AddStormVisual();
+    UPROPERTY(Transient) TArray<TObjectPtr<USceneComponent>> VisualParts;
+};
