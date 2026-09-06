0b702df86d4e15e7d52192e05938ec0d8273412d
ali12hhh-oss <ali12.hhh@gmail.com>
2026-09-07 00:30:33 +0300

    cinematic: expose action director on hero

--- Source/ALSAEQA/Player/ALSAEQACharacter.h
+++ Source/ALSAEQA/Player/ALSAEQACharacter.h
@@ -12,6 +12,7 @@ class UALSAEQAThunderChargeComponent;
 class UALSAEQALegacyComponent;
 class UALSAEQARidingComponent;
 class UALSAEQAVisualAssetComponent;
+class UALSAEQACinematicDirector;
 class USpringArmComponent;
 class UCameraComponent;
 class AALSAEQAMountActor;
@@ -44,6 +45,7 @@ public:
     UFUNCTION(BlueprintPure, Category="ALSAEQA|Thunder") UALSAEQAThunderChargeComponent* GetThunderChargeComponent() const { return ThunderChargeComponent; }
     UFUNCTION(BlueprintPure, Category="ALSAEQA|Legacy") UALSAEQALegacyComponent* GetLegacyComponent() const { return LegacyComponent; }
     UFUNCTION(BlueprintPure, Category="ALSAEQA|Visual") UALSAEQAVisualAssetComponent* GetVisualAssetComponent() const { return VisualAssetComponent; }
+    UFUNCTION(BlueprintPure, Category="ALSAEQA|Cinematics") UALSAEQACinematicDirector* GetCinematicDirector() const { return CinematicDirector; }
 
 protected:
     virtual void BeginPlay() override;
@@ -76,6 +78,7 @@ protected:
     UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="ALSAEQA|Components") TObjectPtr<UALSAEQALegacyComponent> LegacyComponent;
     UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="ALSAEQA|Components") TObjectPtr<UALSAEQARidingComponent> RidingComponent;
     UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="ALSAEQA|Components") TObjectPtr<UALSAEQAVisualAssetComponent> VisualAssetComponent;
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="ALSAEQA|Components") TObjectPtr<UALSAEQACinematicDirector> CinematicDirector;
     UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="ALSAEQA|Movement") float WalkSpeed = 360.0f;
     UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="ALSAEQA|Movement") float SprintSpeed = 620.0f;
     UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="ALSAEQA|Riding") float MountSearchRadius = 450.0f;
