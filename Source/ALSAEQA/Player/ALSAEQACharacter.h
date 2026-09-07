3d4479ea1f6ed6eaf3468d2dfdfa828e8eaa46f2
ali12hhh-oss <ali12.hhh@gmail.com>
2026-09-07 15:56:04 +0300

    Add stage objective component to hero

--- Source/ALSAEQA/Player/ALSAEQACharacter.h
+++ Source/ALSAEQA/Player/ALSAEQACharacter.h
@@ -14,6 +14,7 @@ class UALSAEQARidingComponent;
 class UALSAEQAVisualAssetComponent;
 class UALSAEQACinematicDirector;
 class UALSAEQAStageFlowComponent;
+class UALSAEQAStageObjectiveComponent;
 class USpringArmComponent;
 class UCameraComponent;
 class AALSAEQAMountActor;
@@ -48,6 +49,7 @@ public:
     UFUNCTION(BlueprintPure, Category="ALSAEQA|Visual") UALSAEQAVisualAssetComponent* GetVisualAssetComponent() const { return VisualAssetComponent; }
     UFUNCTION(BlueprintPure, Category="ALSAEQA|Cinematics") UALSAEQACinematicDirector* GetCinematicDirector() const { return CinematicDirector; }
     UFUNCTION(BlueprintPure, Category="ALSAEQA|Progression|Flow") UALSAEQAStageFlowComponent* GetStageFlowComponent() const { return StageFlowComponent; }
+    UFUNCTION(BlueprintPure, Category="ALSAEQA|Progression|Objectives") UALSAEQAStageObjectiveComponent* GetStageObjectiveComponent() const { return StageObjectiveComponent; }
 
 protected:
     virtual void BeginPlay() override;
@@ -82,6 +84,7 @@ protected:
     UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="ALSAEQA|Components") TObjectPtr<UALSAEQAVisualAssetComponent> VisualAssetComponent;
     UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="ALSAEQA|Components") TObjectPtr<UALSAEQACinematicDirector> CinematicDirector;
     UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="ALSAEQA|Components") TObjectPtr<UALSAEQAStageFlowComponent> StageFlowComponent;
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="ALSAEQA|Components") TObjectPtr<UALSAEQAStageObjectiveComponent> StageObjectiveComponent;
     UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="ALSAEQA|Movement") float WalkSpeed = 360.0f;
     UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="ALSAEQA|Movement") float SprintSpeed = 620.0f;
     UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="ALSAEQA|Riding") float MountSearchRadius = 450.0f;
