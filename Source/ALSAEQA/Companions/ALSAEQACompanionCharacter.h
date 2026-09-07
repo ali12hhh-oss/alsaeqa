959843997b42264f754d3485d479742a73b527b3
ali12hhh-oss <ali12.hhh@gmail.com>
2026-09-07 20:10:02 +0300

    Expand companion professional movement states

--- Source/ALSAEQA/Companions/ALSAEQACompanionCharacter.h
+++ Source/ALSAEQA/Companions/ALSAEQACompanionCharacter.h
@@ -29,6 +29,7 @@ class ALSAEQA_API AALSAEQACompanionCharacter : public ACharacter
 
 public:
     AALSAEQACompanionCharacter();
+    virtual void Tick(float DeltaSeconds) override;
 
     UFUNCTION(BlueprintCallable, Category="ALSAEQA|Companion") void SetBehaviorState(EALSAEQACompanionBehaviorState NewState);
     UFUNCTION(BlueprintCallable, Category="ALSAEQA|Companion") void SetCompanionId(FName NewCompanionId);
@@ -36,9 +37,17 @@ public:
     UFUNCTION(BlueprintCallable, Category="ALSAEQA|Companion") bool MarkRescueKnown();
     UFUNCTION(BlueprintCallable, Category="ALSAEQA|Companion") bool RescueCompanion();
     UFUNCTION(BlueprintCallable, Category="ALSAEQA|Companion") bool BeginFamilySearch();
-
     UFUNCTION(BlueprintCallable, Category="ALSAEQA|Companion|Hero Search") bool BeginHelpingHeroFamilySearch();
     UFUNCTION(BlueprintPure, Category="ALSAEQA|Companion|Hero Search") bool IsHelpingHeroFamilySearch() const { return bHelpingHeroFamilySearch; }
+    UFUNCTION(BlueprintCallable, Category="ALSAEQA|Movement") void ToggleCrouch();
+    UFUNCTION(BlueprintCallable, Category="ALSAEQA|Movement") void StartListen();
+    UFUNCTION(BlueprintCallable, Category="ALSAEQA|Movement") void StopListen();
+    UFUNCTION(BlueprintCallable, Category="ALSAEQA|Movement") void PerformRoll();
+    UFUNCTION(BlueprintPure, Category="ALSAEQA|Movement") bool IsListening() const { return bListening; }
+    UFUNCTION(BlueprintPure, Category="ALSAEQA|Movement") bool IsRolling() const { return bRolling; }
+    UFUNCTION(BlueprintImplementableEvent, Category="ALSAEQA|Movement") void PlayMovementPresentation(FName MovementEvent);
+    UFUNCTION(BlueprintImplementableEvent, Category="ALSAEQA|Movement") void PlayRollPresentation();
+    UFUNCTION(BlueprintImplementableEvent, Category="ALSAEQA|Movement") void PlayListenPresentation(bool bStarted);
 
     UFUNCTION(BlueprintCallable, Category="ALSAEQA|Riding") bool Mount(AALSAEQAMountActor* MountActor);
     UFUNCTION(BlueprintCallable, Category="ALSAEQA|Riding") bool Dismount();
@@ -58,6 +67,19 @@ protected:
     UPROPERTY(EditAnywhere, BlueprintReadWrite, SaveGame, Category="ALSAEQA|Companion") FName CompanionId = TEXT("PermanentCompanion");
     UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="ALSAEQA|Companion") float FollowDistance = 220.0f;
     UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="ALSAEQA|Companion") float AssistRange = 900.0f;
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="ALSAEQA|Companion") float FollowStartDistance = 320.0f;
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="ALSAEQA|Companion") float FollowStopDistance = 140.0f;
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="ALSAEQA|Companion") float CompanionWalkSpeed = 330.0f;
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="ALSAEQA|Companion") float CompanionSprintSpeed = 560.0f;
     UPROPERTY(EditAnywhere, BlueprintReadWrite, SaveGame, Category="ALSAEQA|Companion|Hero Search") bool bHelpingHeroFamilySearch = false;
     UPROPERTY(BlueprintReadOnly, Category="ALSAEQA|Companion") EALSAEQACompanionBehaviorState BehaviorState = EALSAEQACompanionBehaviorState::Following;
+
+private:
+    void UpdateFollowMovement(float DeltaSeconds);
+    void FinishRoll();
+    FTimerHandle RollTimerHandle;
+    FTimerHandle RollCooldownTimerHandle;
+    bool bListening = false;
+    bool bRolling = false;
+    bool bRollReady = true;
 };
