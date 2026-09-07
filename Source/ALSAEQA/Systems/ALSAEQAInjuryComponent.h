1bd3be2ca4d5b07402d594a2e3ddf6fc8ec03855
ali12hhh-oss <ali12.hhh@gmail.com>
2026-09-07 20:34:59 +0300

    Enable universal death ragdoll presentation by default

--- Source/ALSAEQA/Systems/ALSAEQAInjuryComponent.h
+++ Source/ALSAEQA/Systems/ALSAEQAInjuryComponent.h
@@ -5,34 +5,11 @@
 #include "ALSAEQAInjuryComponent.generated.h"
 
 UENUM(BlueprintType)
-enum class EALSAEQAInjuryBodyPart : uint8
-{
-    Head,
-    Torso,
-    LeftArm,
-    RightArm,
-    LeftLeg,
-    RightLeg
-};
-
+enum class EALSAEQAInjuryBodyPart : uint8 { Head, Torso, LeftArm, RightArm, LeftLeg, RightLeg };
 UENUM(BlueprintType)
-enum class EALSAEQAInjuryOrgan : uint8
-{
-    Heart,
-    Lungs,
-    Liver,
-    None
-};
-
+enum class EALSAEQAInjuryOrgan : uint8 { Heart, Lungs, Liver, None };
 UENUM(BlueprintType)
-enum class EALSAEQAInjuryState : uint8
-{
-    Healthy,
-    Injured,
-    Critical,
-    KnockedOut,
-    Dead
-};
+enum class EALSAEQAInjuryState : uint8 { Healthy, Injured, Critical, KnockedOut, Dead };
 
 DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FALSAEQAInjuryStateChanged, EALSAEQAInjuryState, State, float, Severity);
 DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FALSAEQAOrganInjured, EALSAEQAInjuryOrgan, Organ);
@@ -43,10 +20,8 @@ UCLASS(ClassGroup=(ALSAEQA), meta=(BlueprintSpawnableComponent))
 class ALSAEQA_API UALSAEQAInjuryComponent : public UActorComponent
 {
     GENERATED_BODY()
-
 public:
     UALSAEQAInjuryComponent();
-
     UFUNCTION(BlueprintCallable, Category="ALSAEQA|Injury") void ApplyBodyPartInjury(EALSAEQAInjuryBodyPart BodyPart, float Severity);
     UFUNCTION(BlueprintCallable, Category="ALSAEQA|Injury") void ApplyOrganInjury(EALSAEQAInjuryOrgan Organ, float Severity);
     UFUNCTION(BlueprintCallable, Category="ALSAEQA|Injury") void SetLimbSevered(EALSAEQAInjuryBodyPart BodyPart, bool bSevered = true);
@@ -55,7 +30,6 @@ public:
     UFUNCTION(BlueprintCallable, Category="ALSAEQA|Injury") void ClearTemporaryInjuries();
     UFUNCTION(BlueprintCallable, Category="ALSAEQA|Injury") void MarkDead();
     UFUNCTION(BlueprintCallable, Category="ALSAEQA|Injury") void ResetAfterDeath();
-
     UFUNCTION(BlueprintPure, Category="ALSAEQA|Injury") EALSAEQAInjuryState GetState() const { return State; }
     UFUNCTION(BlueprintPure, Category="ALSAEQA|Injury") float GetBodyPartSeverity(EALSAEQAInjuryBodyPart BodyPart) const;
     UFUNCTION(BlueprintPure, Category="ALSAEQA|Injury") float GetOrganSeverity(EALSAEQAInjuryOrgan Organ) const;
@@ -63,35 +37,29 @@ public:
     UFUNCTION(BlueprintPure, Category="ALSAEQA|Injury") bool IsKnockedOut() const { return State == EALSAEQAInjuryState::KnockedOut; }
     UFUNCTION(BlueprintPure, Category="ALSAEQA|Injury") bool IsCritical() const { return State == EALSAEQAInjuryState::Critical; }
     UFUNCTION(BlueprintPure, Category="ALSAEQA|Injury") bool IsDead() const { return State == EALSAEQAInjuryState::Dead; }
-
     UFUNCTION(BlueprintImplementableEvent, Category="ALSAEQA|Injury") void PlayInjuryPresentation(EALSAEQAInjuryBodyPart BodyPart, float Severity);
     UFUNCTION(BlueprintImplementableEvent, Category="ALSAEQA|Injury") void PlayDismembermentPresentation(EALSAEQAInjuryBodyPart BodyPart);
     UFUNCTION(BlueprintImplementableEvent, Category="ALSAEQA|Injury") void PlayKnockoutPresentation(bool bStarted);
     UFUNCTION(BlueprintImplementableEvent, Category="ALSAEQA|Injury") void PlayOrganInjuryPresentation(EALSAEQAInjuryOrgan Organ, float Severity);
     UFUNCTION(BlueprintImplementableEvent, Category="ALSAEQA|Injury") void PlayDeathPresentation();
-
     UPROPERTY(BlueprintAssignable, Category="ALSAEQA|Injury") FALSAEQAInjuryStateChanged OnInjuryStateChanged;
     UPROPERTY(BlueprintAssignable, Category="ALSAEQA|Injury") FALSAEQAOrganInjured OnOrganInjured;
     UPROPERTY(BlueprintAssignable, Category="ALSAEQA|Injury") FALSAEQALimbChanged OnLimbChanged;
     UPROPERTY(BlueprintAssignable, Category="ALSAEQA|Injury") FALSAEQAInjuryFatalState OnFatalState;
-
 protected:
     UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="ALSAEQA|Injury") float KnockoutThreshold = 0.90f;
     UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="ALSAEQA|Injury") float CriticalThreshold = 0.75f;
     UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="ALSAEQA|Injury|Visual") bool bAutoHideSeveredLimbBones = true;
     UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="ALSAEQA|Injury|Visual") bool bAutoDisableMovementOnKnockout = true;
-    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="ALSAEQA|Injury|Visual") bool bAutoRagdollOnDeath = false;
-
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="ALSAEQA|Injury|Visual") bool bAutoRagdollOnDeath = true;
     UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="ALSAEQA|Injury|Visual") FName LeftArmBone = TEXT("upperarm_l");
     UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="ALSAEQA|Injury|Visual") FName RightArmBone = TEXT("upperarm_r");
     UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="ALSAEQA|Injury|Visual") FName LeftLegBone = TEXT("thigh_l");
     UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="ALSAEQA|Injury|Visual") FName RightLegBone = TEXT("thigh_r");
-
     UPROPERTY(VisibleInstanceOnly, BlueprintReadOnly, Category="ALSAEQA|Injury") TMap<EALSAEQAInjuryBodyPart, float> BodyPartSeverity;
     UPROPERTY(VisibleInstanceOnly, BlueprintReadOnly, Category="ALSAEQA|Injury") TMap<EALSAEQAInjuryOrgan, float> OrganSeverity;
     UPROPERTY(VisibleInstanceOnly, BlueprintReadOnly, Category="ALSAEQA|Injury") TSet<EALSAEQAInjuryBodyPart> SeveredLimbs;
     UPROPERTY(VisibleInstanceOnly, BlueprintReadOnly, Category="ALSAEQA|Injury") EALSAEQAInjuryState State = EALSAEQAInjuryState::Healthy;
-
 private:
     void RecalculateState();
     void ApplyOwnerStatePresentation();
